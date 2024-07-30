7982b78d97aa338f88b766df9215c87a01adde25
Nelson Apfata <napfata@pagosdigitalesperuanos.pe>
2024-07-30 09:22:28 -0500

    chore: agregado passkey

--- app/app.py
+++ app/app.py
@@ -2,10 +2,19 @@ from pathlib import Path
 
 import uvicorn
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse, JSONResponse
 from jinja2 import Template
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 current_dir = Path(__file__).parent
 
 
