1529acc4859be1638fa40ac1b06f91d40a06b9ce
Apfata <apfata.developer@gmail.com>
2024-07-30 04:08:12 -0500

    feat(simple execute url )

--- app/app.py
+++ app/app.py
@@ -1,13 +1,45 @@
-import google.generativeai as genai
-from fastapi import FastAPI
+from pathlib import Path
+
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from jinja2 import Template
 
 app = FastAPI()
+current_dir = Path(__file__).parent
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(request: Request):
+    print(request.headers)
+    template_path = current_dir / "templates" / "index.html"
+    with open(template_path, "r") as file:
+        template_content = file.read()
+    template = Template(template_content)
+    content = template.render(
+        request=request,
+        message="Hello, World!",
+        url_for=lambda name, **path_params: request.url_for(name, **path_params),
+    )
+
+    return HTMLResponse(content)
+
+
+@app.post("/a")
+async def a(request: Request):
+
+    return {"message": "the method was POST"}
+
+
+@app.put("/b")
+async def b(request: Request):
+
+    return {"message": "the method was PUT"}
 
 
 def get_app():
     return app
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
